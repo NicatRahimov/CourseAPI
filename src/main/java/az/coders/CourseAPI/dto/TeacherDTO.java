fae9b3f81ca712afa290143a65a26833e9efd116
NicatRahimov <nijatrahimov9@gmail.com>
2023-09-13 03:28:58 +0400

    file upload

--- src/main/java/az/coders/CourseAPI/dto/TeacherDTO.java
+++ src/main/java/az/coders/CourseAPI/dto/TeacherDTO.java
@@ -1,10 +1,12 @@
 package az.coders.CourseAPI.dto;
 
 import az.coders.CourseAPI.model.Group;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 @Data
+@AllArgsConstructor
 public class TeacherDTO {
     private Integer id;
     private String name;
