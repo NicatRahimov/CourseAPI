f341a3820434f47c2ea868ad36760a67008e4f7f
NicatRahimov <nijatrahimov9@gmail.com>
2023-09-15 06:28:55 +0400

    jwt

--- src/main/java/az/coders/CourseAPI/model/UserEntity.java
+++ src/main/java/az/coders/CourseAPI/model/UserEntity.java
@@ -1,12 +1,48 @@
 package az.coders.CourseAPI.model;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
 @Entity
 @Data
 @Table(name = "user",schema = "public")
-public class UserEntity {
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY  )
     Integer id;
+    String username;
+    String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
---
+++ src/main/java/az/coders/CourseAPI/util/JwtUtil.java
@@ -0,0 +1,33 @@
+package az.coders.CourseAPI.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private static String SECRET_KEY;
+
+    @Value("${jwt.expirationdate}")
+    private static int EXPIRATION_DATE;
+    public static String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public static Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
