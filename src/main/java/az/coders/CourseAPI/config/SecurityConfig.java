daf44135882ff14324430937aa38e1ec86327ccb
NicatRahimov <nijatrahimov9@gmail.com>
2023-09-15 18:56:20 +0400

    jwt

--- src/main/java/az/coders/CourseAPI/config/SecurityConfig.java
+++ src/main/java/az/coders/CourseAPI/config/SecurityConfig.java
@@ -2,20 +2,13 @@ package az.coders.CourseAPI.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
-//@Bean
-//public UserDetailsService userDetailsService (){
-//    UserDetails admin = User.withUsername("Nicat")
-//            .password("1312")
-//            .roles("ADMIN")
-//            .build();
-//
-//    UserDetails user = User.withUsername("Kamran")
-//            .password("1010")
-//            .roles("USER")
-//            .build();
-//    return new InMemoryUserDetailsManager(admin,user);
-//}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/az/coders/CourseAPI/util/JwtUtil.java
+++ src/main/java/az/coders/CourseAPI/util/JwtUtil.java
@@ -1,33 +1,60 @@
 package az.coders.CourseAPI.util;
 
+import az.coders.CourseAPI.config.SecurityConfig;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.hibernate.annotations.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
+@Component
 public class JwtUtil {
+    @Autowired
+    SecurityConfig securityConfig;
 
     @Value("${jwt.secret}")
     private static String SECRET_KEY;
 
-    @Value("${jwt.expirationdate}")
-    private static int EXPIRATION_DATE;
-    public static String generateToken(UserDetails userDetails) {
+    @Value("${jwt.expirationdate}L")
+    private static Long EXPIRATION_DATE;
+    public String generateToken(UserDetails userDetails) {
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
+                .setIssuer("Nicat")
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_DATE))
+                .signWith(encoder(SECRET_KEY),SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public static Claims extractClaims(String token) {
-        return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
-                .parseClaimsJws(token)
-                .getBody();
-    }
+  public String validateToken(String token){
+
+  }
+
+public String getUsernameToken(String token){
+    Claims claims = Jwts.parser().setSigningKey(encoder(SECRET_KEY)).parseClaimsJws(token).getBody();
+return claims.getSubject();
+}
+
+private Key encoder(String secretKey){
+   return Keys.hmacShaKeyFor(secretKey.getBytes());
+}
+
+
+
+
+
+
 }
